eb4cab9c98af2b977a4586415eaa1b773f973deb
sara <sararbesu@gmail.com>
2023-05-30 11:11:07 +0200

    ejercicio 5

---
+++ xmlAlumnos.py
@@ -0,0 +1,22 @@
+alumnos = {
+    'alumno1': {
+        'id': 1,
+        'nombre': 'Juan',
+        'edad': 18,
+        'curso': 18
+    },
+    'alumno2': {
+        'id': 2,
+        'nombre': 'Maria',
+        'edad': 18,
+        'curso': 12
+    },
+    'alumno3': {
+        'id': 3,
+        'nombre': 'Pedro',
+        'edad': 18,
+        'curso': 12
+    }
+}
+
+print(alumnos)
